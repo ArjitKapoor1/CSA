18ba84d8f1dfb68b0b57aa78a67aac43cbfdba7f
arjit kapoor <arjit47@gmail.com>
2018-09-26 09:16:51 -0700

    First Commit

---
+++ CybersecurityAwarenessPortal/Controllers/ThreatsOverviewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CybersecurityAwarenessPortal.Controllers
+{
+    public class ThreatsOverviewController : Controller
+    {
+        // GET: ThreatsOverview
+        public ActionResult ThreatsOverviewView()
+        {
+            return View();
+        }
+    }
+}
