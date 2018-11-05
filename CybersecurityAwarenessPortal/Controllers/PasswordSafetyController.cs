f72633d2558e8efc67ee3ce6ca68053f1d47e296
arjit kapoor <arjit47@gmail.com>
2018-11-05 12:17:24 -0800

    Updated the portal with Threats Overview and Social Engineering modules content

---
+++ CybersecurityAwarenessPortal/Controllers/PasswordSafetyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CybersecurityAwarenessPortal.Controllers
+{
+    public class PasswordSafetyController : Controller
+    {
+        // GET: PasswordSafety
+        public ActionResult PasswordSafetyView()
+        {
+            return View();
+        }
+    }
+}
